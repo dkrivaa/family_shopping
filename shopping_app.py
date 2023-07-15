e320bf0e56fb288e4d35089d67194d5ffc9be44c
dkrivaa <dannykrivaa@hotmail.com>
2023-07-15 16:55:33 +0300

    Family shopping app

--- shopping_app.py
+++ shopping_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import pandas as pd
+
 
 st.set_page_config(page_title='The family shopping list', layout='wide')
 st.markdown(f'<span style="color: #18448c; font-size: 32px"><b>Shopping list</b></span>'
          , unsafe_allow_html=True)
 st.markdown('___')
+
+
+persons = ['Karen', 'Alex', 'Leanne', 'Yoel' ]
+st.selectbox('Ordered by:', (persons))
+
