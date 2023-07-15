cb9c74f6553196b0fe40fd5bbd3d381a8f423d89
dkrivaa <dannykrivaa@hotmail.com>
2023-07-15 16:36:54 +0300

    Family shopping app

---
+++ shopping_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title='The family shopping list', layout='wide')
+st.markdown(f'<span style="color: #18448c; font-size: 32px"><b>Shopping list</b></span>'
+         , unsafe_allow_html=True)
+st.markdown('___')
