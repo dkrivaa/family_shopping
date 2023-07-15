ff7182cafb873c9952d797ff2fa6a0f9132389c0
dkrivaa <dannykrivaa@hotmail.com>
2023-07-15 20:41:32 +0300

    Family shopping app

--- shopping_app.py
+++ shopping_app.py
@@ -11,10 +11,10 @@ st.markdown(f'<span style="color: #18448c; font-size: 32px"><b>Shopping list</b>
 st.markdown('___')
 
 with st.container():
-    cols = st.columns[1, 1, 12]
-    with cols[0]:
+    col1, col2, col3 = st.columns([1, 1, 12])
+    with col1:
         change_order = st.button('Change order')
-    with cols[1]:
+    with col2:
         new_order = st.button('new order')
 
 # Getting sidebar order menu
