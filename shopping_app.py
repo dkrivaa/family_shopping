199820389e19c610a9c02635a9e9042ed28c7b93
dkrivaa <dannykrivaa@hotmail.com>
2023-07-15 17:24:04 +0300

    Family shopping app

--- shopping_app.py
+++ shopping_app.py
@@ -1,17 +1,18 @@
 import streamlit as st
 import pandas as pd
 
-
+# Basic setup of page
 st.set_page_config(page_title='The family shopping list', layout='wide')
 st.markdown(f'<span style="color: #18448c; font-size: 32px"><b>Shopping list</b></span>'
          , unsafe_allow_html=True)
 st.markdown('___')
 
+# Who can order?
 persons = ['Dad', 'Karen', 'Alex', 'Leanne', 'Yoel']
 
 # New Order
-person = st.selectbox('New order by:', persons)
-product = st.text_input('What would you like to order?')
-amount = st.selectbox('Amount', range(1, 10))
+person = st.sidebar.selectbox('New order by:', persons)
+product = st.sidebar.text_input('What would you like to order?')
+amount = st.sidebar.selectbox('Amount', range(1, 10))
 
 shopping = {'product': product, 'amount': amount, 'person': person}
