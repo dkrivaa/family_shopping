e27c0400bc349507d6217134e19cfc7f25febdcc
dkrivaa <dannykrivaa@hotmail.com>
2023-07-15 17:36:42 +0300

    Family shopping app

---
+++ repo_files/order/order.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+
+# Who can order?
+persons = ['Dad', 'Karen', 'Alex', 'Leanne', 'Yoel']
+
+# New Order
+st.sidebar.title('NEW ORDER')
+person = st.sidebar.selectbox('New order by:', persons)
+product = st.sidebar.text_input('What would you like to order?')
+amount = st.sidebar.selectbox('Amount', range(1, 10))
+
+product_list = []
+amount_list = []
+person_list = []
+
+
+shopping = {'product': product_list, 'amount': amount_list, 'person': person_list}
