92d39bbe5ac9f097c41a44316443625dd0d22efc
dkrivaa <dannykrivaa@hotmail.com>
2023-07-15 18:39:16 +0300

    Family shopping app

--- shopping_app.py
+++ shopping_app.py
@@ -2,12 +2,15 @@ import streamlit as st
 import pandas as pd
 
 from repo_files import repofiles
+from order import order
 
 # Basic setup of page
 st.set_page_config(page_title='The family shopping list', layout='wide')
 st.markdown(f'<span style="color: #18448c; font-size: 32px"><b>Shopping list</b></span>'
          , unsafe_allow_html=True)
 st.markdown('___')
+
+order.order()
 
 # df = repofiles.read_file()
 
