8e614510d1096dd1ce8cdfbe53a256da0e835021
dkrivaa <dannykrivaa@hotmail.com>
2023-07-15 19:47:58 +0300

    Family shopping app

--- shopping_app.py
+++ shopping_app.py
@@ -19,29 +19,4 @@ st.button('new order')
 df = repofiles.read_file()
 table = st.table(df)
 
-
-st.write('Editable DataFrame')
-editable_df = st.dataframe(df)
-
-# Allow users to edit the DataFrame
-if st.button('Apply Changes'):
-    updated_data = {}
-    for column in df.columns:
-        updated_data[column] = []
-        for row_value in editable_df[column]:
-            if isinstance(row_value, str):
-                updated_data[column].append(row_value)
-            else:
-                updated_data[column].append(row_value)
-
-    df = pd.DataFrame(updated_data)
-
-# Display the updated DataFrame
-st.write('Updated DataFrame')
 st.dataframe(df)
-
-
-
-
-
-
