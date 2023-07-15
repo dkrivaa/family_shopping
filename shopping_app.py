7da2a8ef36afdc6e057be6dfcc3dc6f67197976e
dkrivaa <dannykrivaa@hotmail.com>
2023-07-15 17:25:14 +0300

    Family shopping app

--- shopping_app.py
+++ shopping_app.py
@@ -11,6 +11,7 @@ st.markdown('___')
 persons = ['Dad', 'Karen', 'Alex', 'Leanne', 'Yoel']
 
 # New Order
+st.sidebar.title('NEW ORDER')
 person = st.sidebar.selectbox('New order by:', persons)
 product = st.sidebar.text_input('What would you like to order?')
 amount = st.sidebar.selectbox('Amount', range(1, 10))
