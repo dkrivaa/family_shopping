2a1d6a57cd5f3ed302bb227f3ed9ea170a0c3251
dkrivaa <dannykrivaa@hotmail.com>
2023-07-16 12:02:31 +0300

    Family shopping app

--- shopping_app.py
+++ shopping_app.py
@@ -11,7 +11,6 @@ st.markdown(f'<span style="color: #18448c; font-size: 32px"><b>Shopping list</b>
 st.markdown('___')
 
 # EXISTING ORDER LIST
-change_order = st.button('Change order')
 # Reading and showing present shopping list
 df = repofiles.read_file()
 table = st.table(df)
@@ -42,9 +41,10 @@ if submit_order:
     repofiles.save_file(df)
 
 
-if change_order:
-    item = st.selectbox('Which order would you like to change?', df.index)
-    choice = st.radio('What would you like to change?', ['Delete', 'Change Amount'])
+st.markdown(f'<span style="color: #18448c; font-size: 18px"><b>Change Order?</b></span>'
+         , unsafe_allow_html=True)
+item = st.selectbox('Which order would you like to change?', df.index)
+choice = st.radio('What would you like to change?', ['Delete', 'Change Amount'])
 
 
 
