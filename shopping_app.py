d57b3183f1c0cf9e1a9a28686e67d457da37a18a
dkrivaa <dannykrivaa@hotmail.com>
2023-07-16 11:48:40 +0300

    Family shopping app

--- shopping_app.py
+++ shopping_app.py
@@ -45,6 +45,7 @@ if submit_order:
 if change_order:
     item = st.selectbox('Which order would you like to change?', df.index)
     if item:
+        st.write(item)
         choice = st.radio('What would you like to change?', ['Delete', 'Change Amount'])
 
 
