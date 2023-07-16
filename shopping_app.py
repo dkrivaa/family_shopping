2eda2bff8ef8114175ae43fc859f6e724946a9fb
dkrivaa <dannykrivaa@hotmail.com>
2023-07-16 14:11:21 +0300

    Family shopping app

--- shopping_app.py
+++ shopping_app.py
@@ -17,59 +17,56 @@ st.markdown(f'<span style="color: #18448c; font-size: 18px"><b>Existing list</b>
 table = st.table(df)
 st.markdown('___')
 
+# CHANGING EXISTING ORDER
+st.markdown(f'<span style="color: #18448c; font-size: 18px"><b>Change Order?</b></span>'
+            , unsafe_allow_html=True)
 
-cols = st.columns(2)
+item = st.selectbox('Which order would you like to change?', df.index)
+choice = st.radio('What would you like to change?', ['Delete', 'Amount'], index=1)
 
-with cols[0]:
-    # CHANGING EXISTING ORDER
-    st.markdown(f'<span style="color: #18448c; font-size: 18px"><b>Change Order?</b></span>'
-                , unsafe_allow_html=True)
+if choice == 'Delete':
+    df = df.drop(item)
+elif choice == 'Amount':
+    new_amount = st.selectbox('new amount', range(1, 10))
+    df.at[item, 'amount'] = new_amount
+else:
+    pass
 
-    item = st.selectbox('Which order would you like to change?', df.index)
-    choice = st.radio('What would you like to change?', ['Delete', 'Amount'], index=1)
+submit = st.button('Submit changes', type='primary')
+if submit:
+    # Making sure all products are without spaces
+    df['product'] = df['product'].str.replace(' ', '_')
 
-    if choice == 'Delete':
-        df = df.drop(item)
-    elif choice == 'Amount':
-        new_amount = st.selectbox('new amount', range(1, 10))
-        df.at[item, 'amount'] = new_amount
-    else:
-        pass
+    repofiles.del_file()
+    repofiles.save_file(df)
+    st.experimental_rerun()
 
-    submit = st.button('Submit changes', type='primary')
-    if submit:
-        # Making sure all products are without spaces
-        df['product'] = df['product'].str.replace(' ', '_')
+st.markdown('___')
+# NEW ORDER
+st.markdown(f'<span style="color: #18448c; font-size: 18px"><b>New Order?</b></span>'
+            , unsafe_allow_html=True)
+# Who can order?
+persons = ['Dad', 'Karen', 'Alex', 'Leanne', 'Yoel']
+# New Order
+product = st.text_input('What would you like to order?')
+amount = st.selectbox('Amount', range(1, 10))
+person = st.selectbox('New order by:', persons)
+submit_order = st.button('Submit order', type='primary')
 
-        repofiles.del_file()
-        repofiles.save_file(df)
-        st.experimental_rerun()
+if ' ' in product:
+    product = product.replace(' ', '_')
 
-with cols[1]:
-    st.markdown(f'<span style="color: #18448c; font-size: 18px"><b>New Order?</b></span>'
-                , unsafe_allow_html=True)
-    # Who can order?
-    persons = ['Dad', 'Karen', 'Alex', 'Leanne', 'Yoel']
-    # New Order
-    product = st.text_input('What would you like to order?')
-    amount = st.selectbox('Amount', range(1, 10))
-    person = st.selectbox('New order by:', persons)
-    submit_order = st.button('Submit order', type='primary')
+if submit_order:
+    shopping = [product, amount, person]
+    # shopping = {'product': product, 'amount': amount, 'person': person}
+    df.loc[len(df.index)] = shopping
 
-    if ' ' in product:
-        product = product.replace(' ', '_')
+    # Making sure all products are without spaces
+    df['product'] = df['product'].str.replace(' ', '_')
 
-    if submit_order:
-        shopping = [product, amount, person]
-        # shopping = {'product': product, 'amount': amount, 'person': person}
-        df.loc[len(df.index)] = shopping
-
-        # Making sure all products are without spaces
-        df['product'] = df['product'].str.replace(' ', '_')
-
-        repofiles.del_file()
-        repofiles.save_file(df)
-        st.experimental_rerun()
+    repofiles.del_file()
+    repofiles.save_file(df)
+    st.experimental_rerun()
 
 
 
