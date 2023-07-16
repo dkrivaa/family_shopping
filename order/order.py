5c3c38aabc60b52e90476fa76d059b1850dfbcd7
dkrivaa <dannykrivaa@hotmail.com>
2023-07-16 11:46:02 +0300

    Family shopping app

--- order/order.py
+++ order/order.py
@@ -3,8 +3,10 @@ import pandas as pd
 
 from repo_files import repofiles
 
+
 def order_change(item):
     choice = st.radio('What would you like to change?', ['Delete', 'Change Amount'])
+    st.write(choice)
 
 
 
