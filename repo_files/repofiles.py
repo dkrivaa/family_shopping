143195e019d208542622be835f4ea9d496162239
dkrivaa <dannykrivaa@hotmail.com>
2023-07-16 09:26:41 +0300

    Family shopping app

--- repo_files/repofiles.py
+++ repo_files/repofiles.py
@@ -48,7 +48,7 @@ def read_file():  # filename with ''
         content = response.json()['content']
         # Decode the content from base64
         data = base64.b64decode(content).decode('utf-8')
-        data_list = data.split()
+        data_list = data.split(',')
         data_list = data_list[1:]
         st.write(data_list)
         product = []
