f122d65a1a88b9200424d3f154842e39d1b0bb0a
dkrivaa <dannykrivaa@hotmail.com>
2023-07-16 12:04:18 +0300

    Family shopping app

--- shopping_app.py
+++ shopping_app.py
@@ -44,7 +44,7 @@ if submit_order:
 st.markdown(f'<span style="color: #18448c; font-size: 18px"><b>Change Order?</b></span>'
          , unsafe_allow_html=True)
 item = st.selectbox('Which order would you like to change?', df.index)
-choice = st.radio('What would you like to change?', ['Delete', 'Change Amount'])
+choice = st.radio('What would you like to change?', ['Delete', 'Amount'])
 st.write(choice)
 
 
