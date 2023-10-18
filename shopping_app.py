a2da4da168cd2e8fd5cbc3df598a0e46fb64b983
dkrivaa <dannykrivaa@hotmail.com>
2023-10-18 13:05:25 +0300

    update of app - ability to add link to picture

--- shopping_app.py
+++ shopping_app.py
@@ -12,6 +12,7 @@ st.markdown(f'<span style="color: #18448c; font-size: 32px"><b>Shopping list</b>
 st.markdown('___')
 
 df = repofiles.read_file()
+st.write(df)
 try:
     if len(df) > 0:
         df = df.rename_axis(index='Order ID')
